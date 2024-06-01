e7a120cb1dca3111e66e483b43d42b13f909b5f0
Vinicius Baptista <viniciusb_s@hotmail.com>
2024-05-31 22:02:27 -0300

    Adicionando Primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('.mensagemInicio').show();
+
+    $('#buttonInicio').click(function(){
+        $('.mensagemInicio').slideUp(2000);
+        $('.secaoMensagem').slideDown(3000);
+    });
+
+    $('#buttonFotos').click(function(){
+        $('.secaoMensagem').slideUp(1000);
+        $('.secaoFotos').slideDown(7000);
+    });
+
+    $('#buttonFotosFuturas').click(function(){
+        $('.secaoFotos').slideUp(2000);
+        $('.mensagemFuturo').slideDown(6000);
+    });
+
+    $('#buttonEncerrar').click(function(){
+        $('.mensagemFuturo').slideUp(3000);
+        $('.mensagemInicio').slideDown(3000);
+    });
+
+
+})
